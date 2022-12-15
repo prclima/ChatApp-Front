fad9605864bf03115ed5eda95340aa36976ce817
prclima <pedro@futuranet.com.br>
2022-12-15 10:06:05 -0300

    s

--- src/Components/Autenticacao/SingleChat.js
+++ src/Components/Autenticacao/SingleChat.js
@@ -4,7 +4,7 @@ import { Form } from "react-router-dom";
 import { ChatState } from "../../Context/ChatProvider";
 import { api } from "../../API/API.js";
 import Scroll from "../Scroll";
-import  {io}  from "socket.io-client";
+import { io } from "socket.io-client";
 
 const ENDPOINT = "http://localhost:8081";
 let socket, selectedChatComp;
@@ -12,51 +12,51 @@ let socket, selectedChatComp;
 function SingleChat() {
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState([]);
-  const [socketConnected, setSocketConnected] = useState(false)
+  const [socketConnected, setSocketConnected] = useState(false);
 
-  const { user, selectedChat, setSelectedChat, setChats, notification, setNotification} = ChatState();
+  const {
+    user,
+    selectedChat,
+    setSelectedChat,
+    setChats,
+    notification,
+    setNotification,
+  } = ChatState();
 
-  
   async function fetchMessages() {
     try {
       const { data } = await api.get(`api/message/${selectedChat._id}`);
-      
+
       setMessages(data);
-      socket.emit("join chat", selectedChat._id)
-      
+      socket.emit("join chat", selectedChat._id);
     } catch (err) {
       console.log(err);
     }
   }
   //socket.io - conexao
-  useEffect(()=>{
+  useEffect(() => {
     socket = io(ENDPOINT);
     socket.emit("setup", user);
-    socket.on('connection', ()=>setSocketConnected(true))
-  }, [])
+    socket.on("connection", () => setSocketConnected(true));
+  }, []);
 
-    // socket.io - mensagem recebida
-  useEffect(()=>{
-    socket.on("message recieved", (newMessageRecieved) =>{
-      if (! selectedChatComp || selectedChatComp._id !== newMessageRecieved.chat._id){
-        
-      
+  // socket.io - mensagem recebida
+  useEffect(() => {
+    socket.on("message recieved", (newMessageRecieved) => {
+      if (
+        !selectedChatComp ||
+        selectedChatComp._id !== newMessageRecieved.chat._id
+      ) {
       } else {
-        setMessages([...messages, newMessageRecieved])
+        setMessages([...messages, newMessageRecieved]);
       }
-    })
-  
-  })
-
+    });
+  });
 
   useEffect(() => {
     fetchMessages();
     selectedChatComp = selectedChat;
-
   }, [selectedChat]);
-
-
-
 
   async function sendMessage(e) {
     if (e.key === "Enter") {
@@ -67,9 +67,9 @@ function SingleChat() {
           chatId: selectedChat._id,
         });
         setMessages([...messages, data]);
-       
+
         //socket.io - send
-        socket.emit('new message', data);
+        socket.emit("new message", data);
       } catch (err) {
         console.log(err);
       }
@@ -82,11 +82,13 @@ function SingleChat() {
 
   return (
     <>
-     <Text p="4px" fontSize='1.55rem' color='black'>
-      {(selectedChat && user) 
-      ? (user.data._id === selectedChat.users[0]._id ) ? (selectedChat.users[1].name) : selectedChat.users[0].name
-       : ("Selecione uma Conversa...")}
-     </Text>
+      <Text p="4px" fontSize="1.55rem" color="black">
+        {selectedChat && user
+          ? user.data._id === selectedChat.users[0]._id
+            ? selectedChat.users[1].name
+            : selectedChat.users[0].name
+          : "Selecione uma Conversa..."}
+      </Text>
       <Box
         display="flex"
         flexDir="column"
@@ -98,9 +100,9 @@ function SingleChat() {
         borderRadius="lg"
         overflowY="hidden"
       >
-              <div className="messages">
-                <Scroll messages={messages} />
-              </div>
+        <div className="messages">
+          <Scroll messages={messages} />
+        </div>
         <FormControl onKeyDown={sendMessage}>
           <Input
             variant="filled"
--- src/Components/Autenticacao/userListItem.js
+++ src/Components/Autenticacao/userListItem.js
@@ -1,52 +1,36 @@
 import { Avatar } from "@chakra-ui/avatar";
 import { Box, Text } from "@chakra-ui/layout";
-import {Button} from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
 
-
-
-const UserListItem = ({ item, HandleFunction}) => {
-
-
-
-
-
-    return (
-      <Box
-       onClick={HandleFunction}
-        cursor="pointer"
-        bg="#E8E8E8"
-        _hover={{
-          background: "#FF8261", 
-          color: "black",
-        }}
-        w="100%"
-        display="flex"
-        alignItems="center"
-        color="black"
-        px={3}
-        py={2}
-        mb={2}
-        borderRadius="lg"
-      >
-     
-         
-        <Avatar
-          mr={2}
-          size="sm"
-          cursor="pointer"
-          name={item.name}
-          
-        />
-        <Box>
-            
-          <Text>{item.name}</Text>
-          <Text fontSize="xs">
-            <b>Email:</b>
-            {item.email}
-          </Text>
-        </Box>
+const UserListItem = ({ item, HandleFunction }) => {
+  return (
+    <Box
+      onClick={HandleFunction}
+      cursor="pointer"
+      bg="#E8E8E8"
+      _hover={{
+        background: "#FF8261",
+        color: "black",
+      }}
+      w="100%"
+      display="flex"
+      alignItems="center"
+      color="black"
+      px={3}
+      py={2}
+      mb={2}
+      borderRadius="lg"
+    >
+      <Avatar mr={2} size="sm" cursor="pointer" name={item.name} />
+      <Box>
+        <Text>{item.name}</Text>
+        <Text fontSize="xs">
+          <b>Email:</b>
+          {item.email}
+        </Text>
       </Box>
-    );
-  };
+    </Box>
+  );
+};
 
-  export default UserListItem;
+export default UserListItem;
