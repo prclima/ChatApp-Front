a6bf878f41b4a8c0fadccaaefc19ed421d0d59f3
prclima <pedro@futuranet.com.br>
2022-12-15 14:45:51 -0300

    p

--- src/API/API.js
+++ src/API/API.js
@@ -5,7 +5,7 @@ import axios from "axios";
 //     production: "LINK DA API DEPLOYADA AQUI SEM BARRA NO FINAL",
 //   };
 
-const api = axios.create({ baseURL: "quiet-star-3608.fly.dev" });
+const api = axios.create({ baseURL: "https://quiet-star-3608.fly.dev" });
 
 api.interceptors.request.use((config) => {
   const loggedInUserJSON = localStorage.getItem("userInfo");
